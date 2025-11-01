430be1cccf6ad0477bed3ff27807e440a4d71ab6
ConnerDeFeo <ninjanerozz@gmail.com>
2025-11-01 18:22:23 -0400

    making the cors work

--- server/main.py
+++ server/main.py
@@ -18,7 +18,7 @@ else:
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Vite's default port
+    allow_origins=[os.getenv("ALLOW_ORIGINS")],  # Vite's default port
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
